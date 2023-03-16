674e747f6175695141d91ddf86f7ddf0aa21fd91
Mir Adis Ali <93128908+MirAdisAli@users.noreply.github.com>
2023-03-17 01:05:21 +0600

    added

---
+++ Variables.js
@@ -0,0 +1,23 @@
+var bananaPrice = 12;
+console.log(bananaPrice);
+var seenAfter = 12;
+console.log(typeof seenAfter);
+
+var name = "Mark Zuckerburg";
+console.log(typeof name);
+
+var isHot = true;
+var isRich = false;
+console.log(typeof isHot);
+
+var userName = 'Amin';
+console.log(typeof userName);
+
+var promise = "I promise i will ...";
+console.log(promise.toLocaleUpperCase());
+console.log(promise.indexOf('promise'));
+console.log(promise.split(' '));
+
+var name = 'Bangladesh';
+name.indexOf('desh');
+
