01490bab8c20b7ed347ffe9cff5c39e13d541473
Mir Adis Ali <93128908+MirAdisAli@users.noreply.github.com>
2023-03-19 02:27:44 +0600

    added

--- Variables.js
+++ Variables.js
@@ -21,3 +21,18 @@ console.log(promise.split(' '));
 var name = 'Bangladesh';
 name.indexOf('desh');
 
+var number1 = 25;
+var number2 = '15.5';
+number2 = parseInt(number2);
+console.log(number1 + number2)
+
+var number1 = 25;
+var number2 = 15.5;
+number1 = ''+ number1;
+console.log(typeof number1)
+
+var number1 = 0.1;
+var number2 = 0.2;
+var total= number1 + number2;
+total = total.toFixed(1)
+console.log(total)
