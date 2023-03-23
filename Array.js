0f3f56eea5d6b052a2f8af22e355f344942e98bc
Mir Adis Ali <93128908+MirAdisAli@users.noreply.github.com>
2023-03-24 01:50:56 +0600

    added

---
+++ Array.js
@@ -0,0 +1,21 @@
+var friendsAge = [15, 17, 14, 16];
+console.log(friendsAge)
+
+var friendsAge = [15, 17, 14, 16];
+console.log(friendsAge[2])
+
+var friendsAge = [15, 17, 14, 16];
+var sonaliAge = friendsAge[1];
+console.log(sonaliAge)
+
+var friendsAge = [15, 17, 14, 16];
+friendsAge [1] = 21;
+console.log(friendsAge)
+
+var friendsAge = [15, 17, 14, 16];
+var position = friendsAge.indexOf(14);
+console.log(position)
+
+var friendsAge = [15, 17, 14, 16];
+var position = friendsAge.indexOf(135);
+console.log(position)
