df96e6d184aae836bfadfdc8372c8025699fcd89
Mir Adis Ali <93128908+MirAdisAli@users.noreply.github.com>
2023-03-24 00:40:35 +0600

    added

---
+++ Conditionals.js
@@ -0,0 +1,39 @@
+var biscuitPrice = 12
+if(biscuitPrice < 10){
+    console.log("I will eat the biscuits");
+}
+else{
+    console.log("Tea is better");
+}
+
+var biscuitPrice = 8
+if(biscuitPrice < 10){
+    console.log("I will eat the biscuits");
+}
+else{
+    console.log("Tea is better");
+}
+
+var biscuitPrice = 8
+if(biscuitPrice == 8){
+    console.log("I will eat the biscuits");
+}
+else{
+    console.log("Tea is better");
+}
+
+var biscuitPrice = 9
+if(biscuitPrice == 8){
+    console.log("I will eat the biscuits");
+}
+else{
+    console.log("Tea is better");
+}
+
+var biscuitPrice = 15
+if(biscuitPrice != 8){
+    console.log("I will eat the biscuits");
+}
+else{
+    console.log("Tea is better");
+}
