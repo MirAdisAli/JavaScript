95b3376dcb8863d33d9e25fd33009fb481a3d6a0
Mir Adis Ali <93128908+MirAdisAli@users.noreply.github.com>
2023-03-24 00:21:10 +0600

    added

--- Variables.js
+++ Variables.js
@@ -82,9 +82,29 @@ var name = 'Bieber';
 var result = price1 + ' ' + name;
 console.log(result);
 
+var number = -5;
+var absoluteNumber = Math.abs(number);
+console.log(absoluteNumber);
 
+var number = 5.545;
+var result = Math.round(number);
+console.log(result);
 
+var number = 5.099999;
+var result = Math.ceil(number);
+console.log(result);
 
+var number = 5.999999;
+var result = Math.floor(number);
+console.log(result);
+
+var number = 15.00000;
+var result = Math.random();
+console.log(result);
+
+var number = Math.random() * 100;
+var result = Math.round(number);
+console.log(result);
 
 
 
