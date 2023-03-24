b234b0f1bd1c220fe3d41df6f3af18d0b8c0d459
Mir Adis Ali <93128908+MirAdisAli@users.noreply.github.com>
2023-03-24 20:54:06 +0600

    added

--- Loop.js
+++ Loop.js
@@ -10,3 +10,14 @@ while (num <= 15){
     num++;
 }
 
+
+for(var i= 0; i<10; i++){
+    console.log(i);
+}
+
+var nums = [55, 66, 77, 88, 99, 11, 44];
+
+for(var i=0; i<nums.length; i++){
+    var element = nums[i];
+    console.log(element)
+}
